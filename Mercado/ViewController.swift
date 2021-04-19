8e5a73ae6a90a8ef06a56102875e4b70cf669434
mayaradel97 <77214336+mayaradel97@users.noreply.github.com>
2021-04-19 15:49:14 -0700

    firebase pods

--- Mercado/ViewController.swift
+++ Mercado/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var name = "mamam"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ Podfile
@@ -0,0 +1,7 @@
+ platform :ios, '9.0'
+
+target 'Mercado' do
+ pod 'Firebase/Auth'
+pod 'GoogleSignIn'
+
+end
