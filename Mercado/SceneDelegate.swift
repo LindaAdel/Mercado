874e56380342358c4e36b2b74f9bfcff6e768654
mayaradel97 <77214336+mayaradel97@users.noreply.github.com>
2021-04-21 18:02:12 -0700

    comment login handling

--- Mercado/SceneDelegate.swift
+++ Mercado/SceneDelegate.swift
@@ -18,24 +18,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     {
         print("scene delegate")
         guard let windowScnen = (scene as? UIWindowScene) else { return }
-        //initialize window
-        self.window = UIWindow(windowScene: windowScnen)
-        //initialize main storyboard
-        mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            
-            //check if user already signed in
-            if Auth.auth().currentUser != nil
-            {
-                print(Auth.auth().currentUser?.email!)
-                //open home screen
-                initialViewController = mainStoryboard?.instantiateViewController(withIdentifier: "first")as! ViewController
-                  
-                } else {
-                    initialViewController = mainStoryboard?.instantiateViewController(withIdentifier: "login")as! LoginTableViewController
-                }
-         //set initial view controller
-        self.window?.rootViewController = initialViewController
-        self.window?.makeKeyAndVisible()
+//        //initialize window
+//        self.window = UIWindow(windowScene: windowScnen)
+//        //initialize main storyboard
+//        mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+//            
+//            //check if user already signed in
+//            if Auth.auth().currentUser != nil
+//            {
+//                print(Auth.auth().currentUser?.email!)
+//                //open home screen
+//                initialViewController = mainStoryboard?.instantiateViewController(withIdentifier: "first")as! ViewController
+//                  
+//                } else {
+//                    initialViewController = mainStoryboard?.instantiateViewController(withIdentifier: "login")as! LoginTableViewController
+//                }
+//         //set initial view controller
+//        self.window?.rootViewController = initialViewController
+//        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
